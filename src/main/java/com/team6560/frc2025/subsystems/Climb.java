0fd4f575a8918fc3a0589b11f5a1f4aed1dfecf7
Ashray09 <ashrayg09@gmail.com>
2025-07-22 12:51:34 -0700

    just commited

---
+++ src/main/java/com/team6560/frc2025/subsystems/Climb.java
@@ -0,0 +1,109 @@
+package com.team6560.frc2025.subsystems;
+
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import com.ctre.phoenix6.hardware.CANcoder;
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.NeutralModeValue;
+import com.ctre.phoenix6.configs.Slot0Configs;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.controls.PositionVoltage;
+
+import frc.robot.Constants;
+import frc.robot.ManualControls;
+
+import static frc.robot.utility.NetworkTable.NtValueDisplay.ntDispTab;
+
+import frc.robot.Constants.ClimbConstants;
+
+public class Climb extends SubsystemBase {
+    
+    // public enum State {
+    //     UP,
+    //     DOWN,
+    //     STATIC
+    // };
+
+    
+    private TalonFX motor1;
+    private TalonFX motor2;
+
+    
+    private CANcoder absoluteEncoder;
+    private final double initialEncoderPos = 0;
+    private TalonFXConfiguration fxConfig;
+
+    private static final double CLIMB_UP_PERCENT = 0.65;
+    private static final double CLIMB_DOWN_PERCENT = 0.65;
+
+    private final NetworkTable ntTable = NetworkTableInstance.getDefault().getTable("Climb");
+    private final NetworkTableEntry ntPos = ntTable.getEntry("Position");
+    private final NetworkTableEntry ntUseSoftlimits = ntTable.getEntry("ntUseSoftlimits");
+
+    // public State state;
+
+    public Climb(ManualControls controls) { 
+
+        this.motor1 = new TalonFX(ClimbConstants.MOTOR_1_ID, "Canivore");
+        this.motor2 = new TalonFX(ClimbConstants.MOTOR_2_ID, "Canivore");
+        this.absoluteEncoder = new CANcoder(ClimbConstants.CANCODER_ID, "Canivore");
+
+        this.fxConfig = new TalonFXConfiguration(); 
+        fxConfig.MotorOutput.NeutralMode = NeutralModeValue.Brake;
+ 
+        motor1.getConfigurator().apply(fxConfig);
+        motor2.getConfigurator().apply(fxConfig);
+        ntPos.setDouble(this.getEncoderPos());
+        ntUseSoftlimits.setBoolean(true);
+    }
+
+    @Override
+    public void periodic() {
+        ntPos.setDouble(this.getEncoderPos());
+    }
+
+    public void resetEncoderPos() {
+        this.motor1.setPosition(0);
+        this.motor2.setPosition(0);
+        this.absoluteEncoder.setPosition(0);
+    }
+
+    public double getMotor1Pos() {
+        return this.motor1.getPosition().getValueAsDouble();
+    }
+
+    public double getMotor2Pos() { // reversed
+        return -this.motor2.getPosition().getitValueAsDouble();
+    }
+
+    public double getEncoderPos() {
+        return this.absoluteEncoder.getAbsolutePosition().getValueAsDouble();
+    }
+
+    public double getSpeed() {
+        return this.motor1.get();
+    }
+
+    public void stop() {
+        this.motor1.set(0);
+        this.motor2.set(0);
+    }
+
+    public void movedown() {
+        if (!this.ntUseSoftlimits.getBoolean(true) || this.getEncoderPos() > ClimbConstants.LOWER_SOFT_BOUND) {
+            this.motor1.set(-CLIMB_DOWN_PERCENT);
+            this.motor2.set(CLIMB_DOWN_PERCENT); } else {
+                stop();
+            }
+    }
+
+    public void moveup() {
+        if (!this.ntUseSoftlimits.getBoolean(true) || this.getEncoderPos() < ClimbConstants.UPPER_SOFT_BOUND) {
+            this.motor1.set(CLIMB_UP_PERCENT);
+            this.motor2.set(-CLIMB_UP_PERCENT); } else {
+                stop();
+                        }
+    }
+}
