784f6f94eb826f35c414d55db8ad53db30bccc0c
Aneya Patel <aneya.patel@gmail.com>
2025-09-05 18:02:31 -0700

    smartdash

--- src/main/java/com/team6560/frc2025/subsystems/BallGrabber.java
+++ src/main/java/com/team6560/frc2025/subsystems/BallGrabber.java
@@ -35,15 +35,15 @@ public class BallGrabber extends SubsystemBase {
 
 
 public void periodic(){
-    SmartDashboard.putNumber("Ball Grabber Current", grabberMotor.getOutputCurrent());
-    SmartDashboard.putNumber("Ball Grabber Voltage", grabberMotor.getBusVoltage());
-    SmartDashboard.putNumber("Ball Grabber Speed", grabberMotor.get());
-    SmartDashboard.putNumber("Ball Grabber Duty Cycle", getMotorVelocity());
+    SmartDashboard.setDefaultNumber("Ball Grabber Current", grabberMotor.getOutputCurrent());
+    SmartDashboard.setDefaultNumber("Ball Grabber Voltage", grabberMotor.getBusVoltage());
+    SmartDashboard.setDefaultNumber("Ball Grabber Speed", grabberMotor.get());
+    SmartDashboard.setDefaultNumber("Ball Grabber Duty Cycle", getMotorVelocity());
     if((grabberMotor.getOutputCurrent() > GAMEPEICE_CURRENT && grabberMotor.getOutputCurrent() < MAX_CURRENT_RUNNING)){
        // grabberMotor.set(-0.1);
-        SmartDashboard.putBoolean("Ball Detected", true);
+        SmartDashboard.setDefaultBoolean(getName() + " Ball Detected", true);
     } else if (grabberMotor.getOutputCurrent() < GAMEPEICE_CURRENT) {
-        SmartDashboard.putBoolean("Ball Detected", false);
+        SmartDashboard.setDefaultBoolean(getName() + " Ball Detected", false);
         // grabberMotor.set(0.1);
     }
     // else{
